37ab944db20b13db4447cd5c553243bd1bfac58d
Горлов Максим <gorlov.maxim@gmail.com>
2019-08-31 14:06:59 +0300

    Fixing requests proxy

--- observer/configuration/netproxy.py
+++ observer/configuration/netproxy.py
@@ -1,27 +1,31 @@
 from observer import st
-
+import urllib.parse
 
 class NetProxy:
     def __init__(self):
-        self._requests = None
+        self._requests_proxies = None
 
     def requests_proxy(self):
-        if not self._requests:
-            self._requests = {}
+        if not self._requests_proxies:
+            self._requests_proxies = {}
             proxy = st.ST.options()['proxy']
             if proxy['enabled']:
+                prepared_proxy = ""
                 if 'user' in proxy:
-                    self._proxies = {
-                        proxy['type']: 'socks5://{0}:{1}@{2}:{3}'.format(
-                            proxy['user'],
-                            proxy['pass'],
-                            proxy['host'],
-                            proxy['port'])
-                    }
+                    prepared_proxy = '{0}://{1}:{2}@{3}:{4}'.format(
+                                        proxy['type'],
+                                        urllib.parse.quote(proxy['user']),
+                                        urllib.parse.quote(proxy['pass']),
+                                        proxy['host'],
+                                        proxy['port'])
                 else:
-                    self._proxies = {
-                        proxy['type']: 'socks5://{0}:{1}'.format(
-                            proxy['host'],
-                            proxy['port'])
-                    }
-        return self._requests
+                    prepared_proxy = '{0}://{1}:{2}'.format(
+                                        proxy['type'],
+                                        proxy['host'],
+                                        proxy['port'])
+                self._requests_proxies = {
+                    'http': prepared_proxy,
+                    'https': prepared_proxy,
+                }
+            st.ST.debugger().print("Proxyes: ", self._requests_proxies)
+        return self._requests_proxies
